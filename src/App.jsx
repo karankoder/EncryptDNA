1b728e4b7ee35f900a451dcf6bdb233912c39361
karankoder <karankr.das.cse22@iitbhu.ac.in>
2024-12-14 12:47:15 +0530

    chore: initialize project structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+
+const App = () => {
+  return (
+    <div className="App w-[100vw] h-[100vh] overflow-x-hidden overflow-y-scroll no-scrollbar">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+};
+
+export default App;
